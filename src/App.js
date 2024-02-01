525060b0dbc132ad9772184f1b0eb010e839bc37
MilosJovic23 <milos.mj992@gmail.com>
2024-02-01 10:59:18 +0100

    * css-bootstrap changes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 
 import "bootstrap/dist/css/bootstrap.min.css"
 import Weather from "./Components/Weather"
+import "./style.css"
 function App(){
 
 
     return(
         <>
-        <Weather/>
+            <div className="container w-100 d-flex justify-content-center flex-column align-items-center mt-5">
+              <div className="addCityInput w-50 d-flex align-items-center h-100 justify-content-center gap-1 rounded-2 flex-column"><Weather/>  </div>
+            </div>
+
         </>
     )
 }
--- src/Components/Weather.js
+++ src/Components/Weather.js
@@ -15,11 +15,11 @@ function Weather(){
             return
         }
 
-        let newCity=[{
+        let newCity={
             City:city,
             Country:country,
             Temperature:temperature,
-        }]
+        }
            setCities(prev=>[...prev,newCity])
             console.log(cities)
         }
@@ -28,25 +28,36 @@ function Weather(){
 
     return(
         <>
+            <div className="w-75 d-flex gap-2">
+                <input className="form-control w-25" type="text" placeholder="City" onInput={(e) => {
+                    setCity(e.target.value)
+                }}/>
+                <input className="form-control w-25" type="text" placeholder="Country" onInput={(e) => {
+                    setCountry(e.target.value)
+                }}/>
+                <input className="form-control w-25" type="number" placeholder="Temperature in C˚" onInput={(e) => {
+                    setTemperature(e.target.value)
+                }}/>
+                <button className="btn btn-outline-dark text-white" onClick={addCity}>Add City</button>
 
-            <input type="text" placeholder="City" onInput={(e) => {
-                setCity(e.target.value)
-            }}/>
-            <input type="text" placeholder="Country" onInput={(e) => {
-                setCountry(e.target.value)
-            }}/>
-            <input type="number" placeholder="Temperature in C˚" onInput={(e) => {
-                setTemperature(e.target.value)
-            }}/>
-            <button onClick={addCity}>Add City</button>
 
-            <div>
-               {cities.map((city,)=>(
-                   <p>{city.City}</p>))}
-                {cities.map((country)=>(
-                    <p>{country.Country}</p>))}
-                {cities.map((temp)=>(
-                    <p>{temp.Temperature}</p>))}
+            </div>
+
+
+            <div className="w-75 h-50 d-flex gap-3 text-white citiesData">
+                <div className="mb-0">
+                    {cities.map((city) => (
+                        <p>{city.City}</p>))}
+                </div>
+                <div>
+                    {cities.map((country) => (
+                        <p>{country.Country}</p>))}
+                </div>
+                <div>
+                    {cities.map((temp) => (
+                        <p>{temp.Temperature}</p>))}
+                </div>
+
             </div>
         </>
     )
